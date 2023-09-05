be0e97c4b809c53d97f2b161cfdf947622e70826
MiguelMarcola <pedromm65@hotmail.com.br>
2023-09-05 15:10:35 -0300

    Adicionando DTO na camada web

--- src/main/java/com/pedrohlimadev/parkingapi/web/controller/UsuarioController.java
+++ src/main/java/com/pedrohlimadev/parkingapi/web/controller/UsuarioController.java
@@ -2,6 +2,9 @@ package com.pedrohlimadev.parkingapi.web.controller;
 
 import com.pedrohlimadev.parkingapi.entity.Usuario;
 import com.pedrohlimadev.parkingapi.service.UsuarioService;
+import com.pedrohlimadev.parkingapi.web.dto.UsuarioCreateDto;
+import com.pedrohlimadev.parkingapi.web.dto.UsuarioResponseDto;
+import com.pedrohlimadev.parkingapi.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -18,15 +21,15 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
-        Usuario user = usuarioService.salvar(usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable long id) {
+    public ResponseEntity<UsuarioResponseDto> getById(@PathVariable long id) {
         Usuario user = usuarioService.buscarPorId(id);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
     @PatchMapping ("/{id}")
---
+++ src/main/java/com/pedrohlimadev/parkingapi/web/dto/UsuarioCreateDto.java
@@ -0,0 +1,12 @@
+package com.pedrohlimadev.parkingapi.web.dto;
+
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class UsuarioCreateDto {
+
+    private String username;
+    private String password;
+
+}
