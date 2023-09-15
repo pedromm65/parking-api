596f1864c73ae6adab596344b46666bd25a15471
Pedro Lima <pedromm65@hotmail.com.br>
2023-09-15 17:47:31 -0300

    Configurando O MYSQL para teste e criando o teste de criação de usuarios

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,3 +1,4 @@
-insert into USUARIOS (id, username, password, role) values (100, "email0@email.com", "123456", "ROLE_ADMIN");
-insert into USUARIOS (id, username, password, role) values (101, "emai1@email.com", "123456", "ROLE_CLIENTE");
-insert into USUARIOS (id, username, password, role) values (102, "email2@email.com", "123456", "ROLE_CLIENTE");
+insert into USUARIOS (id, username, password, role)
+values (100, "ana@email.com", "123456", "ROLE_ADMIN"),
+       (101, "bia@email.com", "123456", "ROLE_CLIENTE"),
+       (102, "jade@email.com", "123456", "ROLE_CLIENTE");
